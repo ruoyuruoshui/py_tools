f7286a127e722dc23a9fa216f333a17bda118dd2
jiquan.chen <jiquan.chen@nio.com>
2021-12-09 18:27:15 +0800

    suport dark

--- vd/vd_tool.py
+++ vd/vd_tool.py
@@ -17,7 +17,7 @@ name_to_amend = [
         ("/ ", "_"),
         ("／", "_"),
         ("__", "_"),
-        ("12v", "12v_"),
+        ("check1", "check"),
         ]
 
 def amend_name(old_name):
@@ -29,24 +29,29 @@ def amend_name(old_name):
     return new_name
     
 
-def rename_vd(vd_path):
-    file_list = os.listdir(vd_path)
+def rename_vd(tmp_path, vd_path):
+    file_list = os.listdir(tmp_path)
     n = 0
     
     for vd in file_list:
+        if os.path.isdir(vd):
+            continue
         new_name = amend_name(file_list[n])
             
-        old_name_with_path = vd_path + os.sep + file_list[n]
-        new_name_with_path = vd_path + os.sep + "lg_widget_core_" + new_name.lower()
+        old_name_with_path = tmp_path + os.sep + file_list[n]
+        to_path = vd_path
+        if new_name.find("dark.xml") == len(new_name) - len("dark.xml"):
+            to_path = vd_path + os.sep + "dark"
+        new_name_with_path = to_path + os.sep + "lg_widget_core" + new_name.lower()
         os.rename(old_name_with_path, new_name_with_path)
         #print(new_name_with_path + "\n")
         n += 1
 
-def svg_2_vd(svg_path, out_vd_path):
-    cmd = "./vd/vd-tool/bin/vd-tool -c -in " + svg_path + " -out " + out_vd_path
+def svg_2_vd(svg_path, tmp_path, out_vd_path):
+    cmd = "./vd/vd-tool/bin/vd-tool -c -in " + svg_path + " -out " + tmp_path
     print(cmd)
     os.system(cmd)
-    rename_vd(out_vd_path)
+    rename_vd(tmp_path, out_vd_path)
 
 if __name__ == "__main__":
     svg_path = sys.argv[1]
@@ -54,5 +59,9 @@ if __name__ == "__main__":
     if os.path.exists(out_vd_path):
         shutil.rmtree(out_vd_path)
     os.makedirs(out_vd_path)
+    dark_path = out_vd_path + os.sep+ "/dark"
+    tmp_path = out_vd_path + os.sep+ "/tmp"
+    os.makedirs(dark_path)
+    os.makedirs(tmp_path)
 
-    svg_2_vd(svg_path=svg_path, out_vd_path=out_vd_path)
+    svg_2_vd(svg_path=svg_path, tmp_path = tmp_path, out_vd_path=out_vd_path)
